7bc460772ff35cfc172bead314da312c5704f2df
Morteza Soleimani <msoleimani1992@gmail.com>
2024-05-16 18:55:35 +0330

    add c-n c-p keymaps to select next/prev item in auto completion menu

--- lua/lazy-nvim/plugins/nvim-cmp.lua
+++ lua/lazy-nvim/plugins/nvim-cmp.lua
@@ -54,8 +54,10 @@ return { -- Autocompletion
 			-- No, but seriously. Please read `:help ins-completion`, it is really good!
 			mapping = cmp.mapping.preset.insert({
 				-- Select the [n]ext item
+				["<C-n>"] = cmp.mapping.select_next_item(),
 				["<C-j>"] = cmp.mapping.select_next_item(),
 				-- Select the [p]revious item
+				["<C-p>"] = cmp.mapping.select_prev_item(),
 				["<C-k>"] = cmp.mapping.select_prev_item(),
 
 				-- Scroll the documentation window [b]ack / [f]orward
@@ -66,17 +68,7 @@ return { -- Autocompletion
 				--  This will auto-import if your LSP supports it.
 				--  This will expand snippets if the LSP sent a snippet.
 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-
-				-- If you prefer more traditional completion keymaps,
-				-- you can uncomment the following lines
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<Tab>"] = cmp.mapping.select_next_item(),
-				["<S-Tab>"] = cmp.mapping.select_prev_item(),
-
-				-- Manually trigger a completion from nvim-cmp.
-				--  Generally you don't need this, because nvim-cmp will display
-				--  completions whenever it has completion options available.
-				["<C-Space>"] = cmp.mapping.complete({}),
 
 				-- Think of <c-l> as moving to the right of your snippet expansion.
 				--  So if you have a snippet that's like:
